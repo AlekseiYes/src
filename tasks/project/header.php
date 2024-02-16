94c4db899bebf3953e4d3d1be995bc04d2d27b58
AlekseiYes <aleksei.ossipov@student.hamk.fi>
2024-02-16 21:00:39 +0200

    Update header.php

--- tasks/project/header.php
+++ tasks/project/header.php
@@ -2,7 +2,15 @@
 <html>
 <head>
     <title>Portfolio Project</title>
-    <!-- Add your CSS, JavaScript, etc. here -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+crossorigin="anonymous"></script>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <link rel="stylesheet" type="text/css" href="header.css">
+    <link rel="stylesheet" type="text/css" href="styling-phuoc.css">
+
 </head>
 <body>
     <header>
