3fcc15b76eeea1eb2651ef54b6944ca3a5daeb00
AlekseiYes <aleksei.ossipov@student.hamk.fi>
2024-02-24 01:55:55 +0200

    Added telephone number in the form, made it require the "+" in the beginning.

--- tasks/project/contact.php
+++ tasks/project/contact.php
@@ -3,12 +3,10 @@
     $title ='G6 | Portfolio';
     $hero_title = "We are G6";
     $description = "Welcome to G6! We are a passionate and dedicated team of IT student and professionals with a keen interest in software development.Our commitment to delivering high-quality results, coupled with a love for continuous learning, has driven our professional growth";
-
-
+    session_start();
+    
     include "header.php"?>
-    <link rel="stylesheet" type="text/css" href="styles.css">
-    <link rel="stylesheet" type="text/css" href="header.css">
-    <link rel="stylesheet" type="text/css" href="styling-phuoc.css">
+
     
         <div class="content">   
         <div class="container-lg py-5">
@@ -48,24 +46,69 @@
                         <h1 class="title text-center pt-3">
                             Leave Inquiry   
                         </h1>
-                        
-                        <form class="px-3 py-3" method="post" action="process.php"><br>
+                        <h2>
+                        <?php
+                        if (isset($_SESSION['status']) && $_SESSION['status'] == true) {
+                            echo 'Your data was recorded!';
+                            unset($_SESSION['status']);
+                        } else if (isset($_SESSION['status'])){
+                            $error = $_SESSION['error'];
+                            echo 'Something went wrong. ';
+                            
+                            echo $_SESSION['error'];
+                            unset($_SESSION['status']);
+                            unset($_SESSION['error']);
+                        }
+                        ?>
+                        </h2>
+                        <form class="px-3 py-3" method="post" action="process.php" onsubmit="return validateNumber()"><br>
                             <div class="form-group row pb-3 ">
                               <label for="inputEmail" class="col-sm-2 col-form-label">Email</label>
                               <div class="col-sm-10">
                                 <input type="Email" class="form-control" name="email" id="inputEmail" placeholder="Enter your Email" required>
                               </div>
                             </div>
-                            <div class="form-group row">
+                            <div class="form-group row pb-3">
                               <label for="inputName" class="col-sm-2 col-form-label">Name</label>
                               <div class="col-sm-10">
                                 <input type="text" class="form-control" name="name" id="inputName" placeholder="Enter you Name" required>
                               </div>
                             </div>
+                            <div class="form-group row">
+                                <label for="inputNumber" class="col-sm-2 col-form-label">Telephone</label>
+                                <div class="col-sm-10">
+                                    <input type="text" class="form-control" name="number" id="inputNumber" placeholder="Enter your Telephone number" required>
+                                    
+                                </div>
+                                <span id="numberError"></span>
+                            </div>
+                            <script>
+                            function validateNumber() {
+                                const number = document.getElementById("inputNumber").value;
+                                const errorNumber = document.getElementById("numberError");
+
+                                //console.log("Given String is: " + number);
+                                let check = number.charCodeAt(0);
+                                //console.log(check);
+                                if ((check != 43)){
+                                    numberError.innerHTML = "Number must start with \"+\"";
+                                    return false;
+                                }
+                                else {
+                                    numberError.innerHTML = "";
+                                    return true;
+                                }
+
+                                
+                            }
+
+                            document.getElementById("inputNumber").addEventListener("input", validateNumber);
+
+                            </script>
                             <br>
                             <div class="form-group row" style="text-align: left;">
                                 <div class="col-sm-2"></div>
-                                <div class="col-sm-10"><input class="btn-bottom" name="submit" type="submit" value="Submit"></div>
+                                <div class="col-sm-10"><input class="btn-bottom" name="submit" type="submit" value="Submit" id="submit"></div>
                                 </div>
                             </div>
                             
